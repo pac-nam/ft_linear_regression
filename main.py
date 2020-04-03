428a17d539fc19b1d4417a87f5a5aafdd4f1a42a
pac-nam <thomasbleuse02@gmail.com>
2020-04-03 13:20:34 +0200

    formules need modification

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def estimate_price(km, theta0, theta1):
 	return (km * theta1 + theta0)
 
 def training(file_name):
-	theta0, theta1, cars_number = 0, 0, 0
+	t0, t1 = 0, 0
 	csv = pandas.read_csv(file_name, delimiter=',')
 	if len(csv.columns) != 2:
 		return 0, 0, "invalid file"
@@ -15,14 +15,21 @@ def training(file_name):
 	if (cars_number == 0):
 		return (0,0,"No car")
 	for car in csv.itertuples():
-		theta0 += estimate_price(car.km, theta0, theta1) - car.price
-		theta1 += (estimate_price(car.km, theta0, theta1) - car.price) * car.km
-	theta0 = theta0 * (1/cars_number)
-	theta1 = theta1 * (1/cars_number)
-	return (theta0, theta1, "")
+		print("t0", t0, "| t1", t1, car)
+		if (car.Index == 0):
+			tmpt0, tmpt1 = 0, 0
+		else:
+			tmpt0 = t0 / car.Index
+			tmpt1 = t1 / car.Index
+		print("tmpt0", tmpt0, "| tmpt1", tmpt1)
+		t0 += estimate_price(car.km, tmpt0, tmpt1) - car.price
+		t1 -= (estimate_price(car.km, tmpt0, tmpt1) - car.price) / car.km
+	t0 = t0 / cars_number
+	t1 = t1 / cars_number
+	return (t0, t1, "")
 
 def save_theta(theta0, theta1):
-	print("Theta0 =", theta0, "Theta1 =", theta1)
+	print("Theta0 =", theta0, "\nTheta1 =", theta1)
 	print(estimate_price(150000, theta0, theta1))
 
 if __name__ == "__main__":
