e3ac2425db3bb43eaf0999075b290bdc4e0decab
ayguillo <ayguillo@student.42.fr>
2020-04-03 12:23:42 +0200

    [UPDATE] first test with theta

--- main.py
+++ main.py
@@ -3,28 +3,37 @@
 import pandas
 import sys
 
+def estimate_price(km, theta0, theta1):
+	return (km * theta1 + theta0)
+
 def training(file_name):
-	teta0, teta1, cars_number = 0, 0, 0
+	theta0, theta1, cars_number = 0, 0, 0
 	csv = pandas.read_csv(file_name, delimiter=',')
 	if len(csv.columns) != 2:
 		return 0, 0, "invalid file"
-	try:	csv.km
-			csv.price
-	except AttributeError as error:
-		print("lol", error)
-	print(csv.km)
 	cars_number = len(csv)
-	return 0, 0, ""
+	if (cars_number == 0):
+		return (0,0,"No car")
+	for car in csv.itertuples():
+		theta0 += estimate_price(car.km, theta0, theta1) - car.price
+		theta1 += (estimate_price(car.km, theta0, theta1) - car.price) * car.km
+	theta0 = theta0 * (1/cars_number)
+	theta1 = theta1 * (1/cars_number)
+	return (theta0, theta1, "")
 
-def save_teta(teta0, teta1):
-	print("tetas will be saved here")
+def save_theta(theta0, theta1):
+	print("Theta0 =", theta0, "Theta1 =", theta1)
+	print(estimate_price(150000, theta0, theta1))
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Usage:", sys.argv[0], "<file1>")
-		sys.exit(0)
-	teta0, teta1, error = training(sys.argv[1])
-	if error != "":
-		print(error)
-		sys.exit(0)
-	save_teta(teta0, teta1)
+	try :
+		if len(sys.argv) != 2:
+			print("Usage:", sys.argv[0], "<file1>")
+			sys.exit(0)
+		theta0, theta1, error = training(sys.argv[1])
+		if error != "":
+			print(error)
+			sys.exit(0)
+		save_theta(theta0, theta1)
+	except AttributeError as error:
+		print("Error system : {}".format(error))
