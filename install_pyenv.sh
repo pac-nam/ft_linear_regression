15a1e7669c807f38ef071e6ad7b017988e698b8f
ayguillo <ayguillo@student.42.fr>
2020-04-20 16:28:47 +0200

    [ADD] help to install pyenv

---
+++ install_pyenv.sh
@@ -0,0 +1,9 @@
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+# Decommenter pour ubuntu
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+exec $SHELL
